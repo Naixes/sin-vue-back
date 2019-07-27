38202306762a4b3c3044e1326c8a46db170aded2
Naixes <615411375@qq.com>
2019-07-27 08:17:03 +0800

    parcel

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Button from './Button'
+
+Vue.component('s-button', Button)
+
+new Vue({
+	el: '#app',
+	data: {
+	}
+})
---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+		<button class="s-button">hi</button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.s-button {
+	font-size: var(--font-size);
+	height: var(--button-height);
+	padding: 0 1em;
+
+	background: var(--border-bgc);
+
+	border-radius: var(--button-radius);
+	border: 1px solid var(--border-color);
+	&:hover {
+		border-color: var(--border-hover-color);
+	}
+	&:active {
+		background-color: var(--button-active-bgc);
+	}
+	&:focus {
+		outline: none;
+	}
+}
+</style>
