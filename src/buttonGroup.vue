a0519f4aa917f1d97dff08261bd2b1b58bd76675
Naixes <615411375@qq.com>
2019-07-31 15:06:37 +0800

    button-group

---
+++ src/buttonGroup.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="s-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log(this.$el.children)
+        for(let node of this.$el.children) {
+            const name = node.nodeName.toLowerCase()
+            if(name !== 'button') {
+                console.warn(`s-button-group 的子元素应该是 button 而不是 ${name} `)
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.s-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    .s-button {
+        border-radius: 0;
+        &:first-child {
+            border-top-left-radius: 4px;
+            border-bottom-left-radius: 4px;
+        }
+        &:last-child {
+            border-top-right-radius: 4px;
+            border-bottom-right-radius: 4px;
+        }
+
+        // 防止边框重叠
+        &:not(:first-child) {
+            margin-left: -1px;
+        }
+        &:hover {
+            z-index: 1;
+        }
+    }
+}
+</style>
