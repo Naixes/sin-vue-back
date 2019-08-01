19209c5749d699fa2856df5e26dac845da046c1b
Naixes <615411375@qq.com>
2019-08-01 16:52:20 +0800

    接入travis

--- README.md
+++ README.md
@@ -340,13 +340,11 @@ chai：not，deep，own，NaN
 
 #### travisc持续集成
 
-持续测试，持续交付，持续部署
-
-修改测试脚本
+包括：持续测试，持续交付，持续部署
 
 travisc ci：没有数量限制， circle ci：一个以上收费
 
-阮一峰教程
+阮一峰教程：<http://www.ruanyifeng.com/blog/2017/12/travis_ci_tutorial.html>
 
 ##### 持续测试
 
--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function (config) {
       // start these browsers
       // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
       // 设置浏览器  
-      browsers: ['Chrome'],
+      browsers: ['ChromeHeadless'],
   
   
       // Continuous Integration mode
