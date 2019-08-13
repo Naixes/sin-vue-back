08a1da109f011fb65b6018dcd189a2cc800987ca
Naixes <615411375@qq.com>
2019-08-13 18:38:03 +0800

    row&col：gutter，offset

--- src/col.vue
+++ src/col.vue
@@ -1,17 +1,51 @@
 <template>
-    <div class="s-col">
+    <div 
+        class="s-col" 
+        :class="[`col-${span}`, `offset-${offset}`]"
+        :style="{paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px'}"
+    >
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Col'
+    name: 'Col',
+    props: {
+        // 跨度
+        span: {
+            type: [Number, String]
+        },
+        // 偏移
+        offset: {
+            type: [Number, String]
+        }
+    },
+    data() {
+        return {
+            gutter: 0
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .s-col {
     width: 100%;
+
+    $class-perfix: col-;
+    // scss循环
+    @for $n from 1 through 24 {
+        &.#{$class-perfix}#{$n} {
+            width: ($n / 24) * 100%;
+        }
+    }
+
+    $class-perfix: offset-;
+    @for $n from 1 through 24 {
+        &.#{$class-perfix}#{$n} {
+            margin-left: ($n / 24) * 100%;
+        }
+    }
 }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,25 @@
 <template>
-    <div class="s-row">
+    <!-- 抵消掉row两边的gutter -->
+    <div class="s-row" :style="{marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}">
         <slot></slot>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Row'
+    name: 'Row',
+    props: {
+        // 间距
+        gutter: {
+            type: [Number, String]
+        }
+    },
+    // 向col传递gutter，需要用data接收
+    mounted() {
+        this.$children.forEach(vm => {
+            vm.gutter = this.gutter
+        })
+    }
 }
 </script>
 
