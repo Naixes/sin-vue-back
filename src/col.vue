93c5894b3420ec376b66d1d423d85d08b99e59a4
Naixes <615411375@qq.com>
2019-08-14 15:49:01 +0800

    重构

--- src/col.vue
+++ src/col.vue
@@ -1,13 +1,11 @@
 <template>
     <div class="s-col-wrapper"
-        :class="[`col-${span}`, `offset-${offset}`]"
-				:style="{paddingLeft: gutter / 2 + 'px', paddingRight: gutter / 2 + 'px'}"
+        :class="colClass"
+        :style="colStyle"
     >
-			<div 
-        class="s-col" 
-			>
-					<slot></slot>
-			</div>
+        <div class="s-col">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
@@ -26,7 +24,23 @@ export default {
     },
     data() {
         return {
-            gutter: 0
+            gutter: 0,
+        }
+    },
+    // 写在data中时只会读取一次，要通过计算属性
+    computed: {
+        colClass() {
+            let {span, offset} = this
+            return [
+                span && `col-${span}`,
+                offset && `offset-${offset}`
+            ]
+        },
+        colStyle() {
+            return {
+                paddingLeft: this.gutter / 2 + 'px',
+                paddingRight: this.gutter / 2 + 'px'
+            }
         }
     }
 }
@@ -39,16 +53,16 @@ export default {
 	$class-perfix: col-;
 	// scss循环
 	@for $n from 1 through 24 {
-			&.#{$class-perfix}#{$n} {
-					width: ($n / 24) * 100%;
-			}
+        &.#{$class-perfix}#{$n} {
+            width: ($n / 24) * 100%;
+        }
 	}
 
 	$class-perfix: offset-;
 	@for $n from 1 through 24 {
-			&.#{$class-perfix}#{$n} {
-					margin-left: ($n / 24) * 100%;
-			}
+        &.#{$class-perfix}#{$n} {
+            margin-left: ($n / 24) * 100%;
+        }
 	}
 }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- 抵消掉row两边的gutter -->
-    <div class="s-row" :style="{marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}">
+    <div class="s-row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
@@ -12,6 +12,19 @@ export default {
         // 间距
         gutter: {
             type: [Number, String]
+        }
+    },
+    data() {
+        return {
+        }
+    },
+    // 写在data中时只会读取一次，要通过计算属性
+    computed: {
+        rowStyle() {
+            return {
+                marginLeft: -this.gutter / 2 + 'px',
+                marginRight: -this.gutter / 2 + 'px'
+            }
         }
     },
     // 向col传递gutter，需要用data接收
